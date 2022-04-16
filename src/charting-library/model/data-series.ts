388057c55304102f931c8ed6252046637c1a86fc
Dmitry <dmitrygry@gmail.com>
2022-04-16 22:02:50 +0300

    Ported point classes to typescript.

--- src/charting-library/model/data-series.ts
+++ src/charting-library/model/data-series.ts
@@ -1,30 +1,28 @@
-﻿(function (window) {
+﻿import {Point} from "./point/point";
 
-    var DataSeries = function (data, ordered, name, color) {
-        /// <summary>
-        /// Creates data series with specified data.
-        /// Data series contains data points array,
-        // Name and color
-        /// </summary>
-        /// <param name="data" type="Array">Array with data series data points.</param>
-        /// <param name="ordered" type="Boolean">True, if data series data is ordered. Otherwise, must be false.</param>
-        /// <param name="name" type="String">Series name.</param>
-        /// <param name="color" type="String">Color, used for displaying series.</param>
-        this.data = data;
-        this.ordered = ordered;
-        this.name = name;
-        this.color = color;
+/**
+ * DataSeries with X coordinate of generic type
+ * @template X
+ */
+export class DataSeries<X> {
 
-    }
+  readonly data: Array<Point<X>>;
+  readonly ordered: boolean;
 
-    var p = DataSeries.prototype;
+  name: string;
+  color: string;
 
-    p.data = null;
-    p.ordered = null;
-
-    p.name = null;
-    p.color = null;
-
-    window.DataSeries = DataSeries;
-
-}(window));
+  /**
+   * Represents event occurring on multitouch navigation.
+   * @param {Array<Point<X>>} data - Data series data array.
+   * @param {boolean} ordered - Indicates whether DataSeries points are ordered or not.
+   * @param {string} name - Data series name.
+   * @param {string} color - Data series color.
+   */
+  constructor(data: Array<Point<X>>, ordered: boolean, name: string, color: string) {
+    this.data = data;
+    this.ordered = ordered;
+    this.name = name;
+    this.color = color;
+  }
+}
