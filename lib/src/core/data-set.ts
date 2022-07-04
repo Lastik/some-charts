9fc972178e9e173c27a6fd9cf7cf6b02739e617c
Dmitry <dmitrygry@gmail.com>
2022-07-04 04:07:58 +0300

    working on dataset

--- lib/src/core/data-set.ts
+++ lib/src/core/data-set.ts
@@ -1,11 +1,41 @@
-export class DataSet<TItemType, XDimensionType, YDimensionType> {
+export class DataSet<TItemType,
+  XDimensionType extends number | string | Date,
+  YDimensionType extends number | string | Date> {
 
   public readonly elements: Array<TItemType>;
 
+  private metricsFuncs: { [name: string]: (item: TItemType) => number };
+  private dimensionXFunc: (item: TItemType) => XDimensionType;
+  private dimensionYFunc?: ((item: TItemType) => YDimensionType);
+
+  public readonly dimensionXValues: XDimensionType[];
+  public readonly dimensionYValues: YDimensionType[] | undefined;
+
+
+
   constructor(elements: Array<TItemType>,
-              dimensionX: (item: TItemType) => XDimensionType,
-              dimensionY: (item: TItemType) => YDimensionType,
+              metricsFuncs: {[name: string]: (item: TItemType) => number},
+              dimensionXFunc: (item: TItemType) => XDimensionType,
+              dimensionYFunc?: (item: TItemType) => YDimensionType,
               ) {
     this.elements = elements;
+    this.metricsFuncs = metricsFuncs;
+    this.dimensionXFunc = dimensionXFunc;
+    this.dimensionYFunc = dimensionYFunc;
+
+    this.dimensionXValues = this.elements.map(v => this.dimensionXFunc(v));
+    this.dimensionYValues = this.dimensionYFunc ? this.elements.map(v => this.dimensionYFunc!(v)) : undefined;
+
+    this.values = Map<>
   }
+
+  public get is1D(): boolean {
+    return this.dimensionYFunc === undefined;
+  }
+
+  public get is2D(): boolean {
+    return this.dimensionYFunc !== undefined;
+  }
+
+  public get
 }
