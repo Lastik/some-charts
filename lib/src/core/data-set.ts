6d7b48fbffc1ec145c5ca4bc56c8ea1c83eb3e8f
Dmitry <dmitrygry@gmail.com>
2022-07-06 02:21:14 +0300

    working on chart data set

--- lib/src/core/data-set.ts
+++ lib/src/core/data-set.ts
@@ -4,14 +4,17 @@ export class DataSet<TItemType,
 
   public readonly elements: Array<TItemType>;
 
-  private metricsFuncs: { [name: string]: (item: TItemType) => number };
-  private dimensionXFunc: (item: TItemType) => XDimensionType;
-  private dimensionYFunc?: ((item: TItemType) => YDimensionType);
+  private readonly metricsFuncs: { [name: string]: (item: TItemType) => number };
+  private readonly dimensionXFunc: (item: TItemType) => XDimensionType;
+  private readonly dimensionYFunc?: ((item: TItemType) => YDimensionType);
 
   public readonly dimensionXValues: XDimensionType[];
   public readonly dimensionYValues: YDimensionType[] | undefined;
 
+  private readonly indexByXDimension: Map<number | string, number>;
+  private readonly indexByYDimension: Map<number | string, number>;
 
+  private metricsValues: Map<string, Array<Array<number>>>;
 
   constructor(elements: Array<TItemType>,
               metricsFuncs: {[name: string]: (item: TItemType) => number},
@@ -23,10 +26,46 @@ export class DataSet<TItemType,
     this.dimensionXFunc = dimensionXFunc;
     this.dimensionYFunc = dimensionYFunc;
 
+    this.metricsValues = new Map<string, Array<Array<number>>>();
+
+    let dimensionXValuesSet = [];
+    let dimensionYValuesSet = this.dimensionYFunc ? [] : undefined;
+
+    this.elements.forEach((element, index) => {
+
+      let dimXValue = this.dimensionXFunc(element);
+      let dimYValue = this.dimensionYFunc ? (this.dimensionYFunc!(element)) | undefined;
+
+      //TODO: unique.
+
+      //dimensionXValuesSet.push(dimXValue);
+      //if(dimYValue){
+      //  this.dimensionYValues!.push(dimYValue);
+      //}
+
+      for (let metricName in this.metricsFuncs) {
+        let metricFunc = this.metricsFuncs[metricName];
+        let metricValue = metricFunc(element);
+
+        let metricValues = this.metricsValues.get(metricName) ?? new Array<Array<number>>();
+
+      }
+    });
+
     this.dimensionXValues = this.elements.map(v => this.dimensionXFunc(v));
     this.dimensionYValues = this.dimensionYFunc ? this.elements.map(v => this.dimensionYFunc!(v)) : undefined;
 
-    this.values = Map<>
+    this.indexByXDimension = new Map<number | string, number>();
+
+    this.dimensionXValues.forEach((dimVal, idx) => {
+      this.indexByXDimension.set(DataSet.makePrimitive(dimVal), idx);
+    });
+
+    this.indexByYDimension = new Map<number | string, number>();
+
+    this.dimensionYValues?.forEach((dimVal, idx) => {
+      this.indexByYDimension.set(DataSet.makePrimitive(dimVal), idx);
+    });
   }
 
   public get is1D(): boolean {
@@ -37,5 +76,13 @@ export class DataSet<TItemType,
     return this.dimensionYFunc !== undefined;
   }
 
-  public get
+  public getMetricValue(metricName: string, x: XDimensionType, y?: YDimensionType): number {
+
+  }
+
+  private static makePrimitive(value: number | string | Date): number | string {
+    if (value instanceof Date) {
+      return value.getTime();
+    } else return value;
+  }
 }
