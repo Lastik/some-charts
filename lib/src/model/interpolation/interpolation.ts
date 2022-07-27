e66dc6651a2bd2196d7dfe35449687dc84c6f558
Dmitry <dmitrygry@gmail.com>
2022-07-28 02:28:12 +0300

    Working on plots

--- lib/src/model/interpolation/interpolation.ts
+++ lib/src/model/interpolation/interpolation.ts
@@ -1,3 +1,15 @@
-export interface Interpolation{
+import {Range} from '../range';
 
+export abstract class Interpolation<InputType, OutputType> {
+  inputRange: Range<InputType>;
+  outputRange: Range<OutputType>;
+  clippingOutputRange: Range<OutputType>
+
+  constructor(inputRange: Range<InputType>, outputRange: Range<OutputType>, clippingOutputRange: Range<OutputType>) {
+    this.inputRange = inputRange;
+    this.outputRange = outputRange;
+    this.clippingOutputRange = clippingOutputRange;
+  }
+
+  abstract apply(value: InputType): OutputType;
 }
