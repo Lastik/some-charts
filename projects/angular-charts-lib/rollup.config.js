02cdd771912aa12050500aab3ece93c2b63fd875
Dmitry <dmitrygry@gmail.com>
2022-08-21 22:59:15 +0300

    working on library build

--- projects/angular-charts-lib/rollup.config.js
+++ projects/angular-charts-lib/rollup.config.js
@@ -1,7 +1,7 @@
 import dts from 'rollup-plugin-dts';
 import esbuild from 'rollup-plugin-esbuild';
 
-const name = require('./package.json').name;
+const name = require('./package.json').main.replace(/\.js$/, '');
 
 const bundle = config => ({
   ...config,
